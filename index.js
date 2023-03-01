c5ef1922465211d84bb8205d71199adb16668265
Ilin Alexey <ilin.alexey96@gmail.com>
2023-03-02 01:58:05 +0700

    add form edit

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const buttonEdit = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup')
+const profileName = document.querySelector('.profile__title');
+const profileJob = document.querySelector('.profile__subtitle');
+const formElement = document.querySelector('.popup__form');
+const nameInput = document.querySelectorAll('.popup__input')[0];
+const jobInput = document.querySelectorAll('.popup__input')[1];
 
 buttonEdit.addEventListener('click', function() {
     document.querySelector('.popup').classList.add('popup_opened')
@@ -7,10 +13,16 @@ buttonEdit.addEventListener('click', function() {
 const popupClosed = document.querySelector('.popup__escape-button');
 
 popupClosed.addEventListener('click', function() {
-    const popup = document.querySelector('.popup')
     if (popup.classList.contains('popup_opened')) {
         popup.classList.remove('popup_opened')
-    } else {
-        alert("class popup_opened not found");
     }
 })
+
+function handleFormSubmit(evt) {
+    evt.preventDefault(); 
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+    popup.classList.remove('popup_opened')
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
